98538dd940d4d7a80add40b93ccbaeacc04fa441
Marcos Henrique <96560744+sniperhenrique@users.noreply.github.com>
2023-02-18 23:37:48 -0300

    Update ping.js

--- Comandos/Utilitarios/ping.js
+++ Comandos/Utilitarios/ping.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js")
 
 module.exports = {
-  name: "ping", // Coloque o nome do comando
-  description: "[Utilitarios] Veja o ping do bot.", // Coloque a descrição do comando
+  name: "ping", 
+  description: "[Utilitarios] Veja o ping do bot.", 
   type: Discord.ApplicationCommandType.ChatInput,
 
   run: async (client, interaction) => {
